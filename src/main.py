6320fe27594fc17d00ac71073b7e23123ac084c6
JackeyF <2675299845@qq.com>
2022-07-22 17:24:57 -0400

    Add maps

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from sfm_simulator import SFMSimulator
 
-sfm_simulator = SFMSimulator("maps/mikawalab.pgm", "maps/mikawalab.yaml",
+sfm_simulator = SFMSimulator("maps/sample_map.pgm", "maps/sample_map.yaml",
                              zoom=2, dt=0.05, sim_speed=0.2)
 sfm_simulator.debug()
 
